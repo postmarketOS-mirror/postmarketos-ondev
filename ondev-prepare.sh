3a581c594459e3a746d6300e2335d7a402f49dda
Oliver Smith <ollieparanoid@postmarketos.org>
2020-06-10 14:53:11 +0200

    ondev-prepare.sh: stop if arg count is wrong
    Before the script will actually do something, add a sanity check that
    prevents unintended use outside of 'pmbootstrap install --ondev'.

--- ondev-prepare.sh
+++ ondev-prepare.sh
@@ -8,11 +8,19 @@
 # * store the channel properties somewhere to display them
 # * transform rootfs.img
 
+if [ "$#" -ne 5 ]; then
+	echo "ERROR: do not run this script manually."
+	echo "It's only meant to be called during 'pmbootstrap install --ondev'"
+	exit 1
+fi
+
 channel="$1"
 channel_description="$2"
 channel_branch_pmaports="$3"
 channel_branch_aports="$4"
 channel_mirrordir_alpine="$5"
+
+set -x
 
 # do nothing for now
 exit 0
